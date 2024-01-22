5682e77084d27d90ea48b67174c429aca80efdb7
Khanh <nbk456123@gmail.com>
2024-01-22 22:02:37 +0700

    them file de convert sang sqlite

---
+++ VietAnh/convertjs.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import sqlite3, json
+from datetime import datetime
+
+
+def convert_price(price_str):
+    if pd.isna(price_str):
+        return 0
+    else:
+        return float(price_str)
+
+
+def convert_area(area_str):
+    if pd.isna(area_str):
+        return 0
+    if "-" in area_str:
+        start, end = map(
+            float,
+            area_str.replace("m2", "").replace(".", "").replace(",", "").split("-"),
+        )
+        return (start + end) / 2
+    else:
+        return float(area_str.replace("m2", "").replace(".", "").replace(",", ""))
+
+
+def extract_location(address):
+    parts = address.split(", ")
+    district = parts[-2].replace("Quận", "").replace("Huyện", "").strip()
+    city = parts[-1].replace("Tỉnh", "").replace("Thành phố", "").strip()
+    return district, city
+
+
+conn = sqlite3.connect("demo_web/test.db")
+cursor = conn.cursor()
+
+with open("VietAnh/Bds1.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+for item in data:
+    id, title, price, area = (
+        item.get("id", ""),
+        item.get("title", ""),
+        convert_price(item.get("price", "")),
+        convert_area(item.get("area", "")),
+    )
+
+    if area > 10:
+        time_str = item.get("time", "")
+        time = datetime.strptime(time_str, "%d/%m/%Y") if time_str else None
+
+        address = item.get("address", "")
+        district, city = extract_location(address)
+
+        price_per_m2 = round(price / area) if price and area and area != 0 else 0
+        description, url, image, source = (
+            item.get("description", ""),
+            item.get("url", ""),
+            item.get("image", ""),
+            "https://homedy.com/",
+        )
+
+        cursor.execute(
+            "INSERT INTO products (prid, title, time_published, district, city, price, acreage, price_per_m2, description, product_url, main_image, source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (
+                id,
+                title,
+                time.date() if time else None,
+                district,
+                city,
+                price,
+                area,
+                price_per_m2,
+                description,
+                url,
+                image,
+                source,
+            ),
+        )
+
+conn.commit()
+conn.close()
+
+print(
+    f"Data from 'VietAnh/Bds1.json' has been imported into SQLite database 'demo_web/test.db'."
+)
--- quan/convertjs.py
+++ quan/convertjs.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import sqlite3
-import os, json
+import json
 
 
 def convert_price(price_str):
     if pd.isna(price_str) or "Thỏa thuận" in price_str:
         return 0
     elif "triệu" in price_str:
-        return float(price_str.replace(" triệu", "")) * 1e6
+        result = float(price_str.replace(" triệu", "")) * 1e6
     elif "tỷ" in price_str:
-        return float(price_str.replace(" tỷ", "")) * 1e9
+        result = float(price_str.replace(" tỷ", "")) * 1e9
     else:
-        return 0
+        result = 0
+
+    return 0 if result >= 1e11 else result
 
 
 def convert_acreage(acreage_str):
-    if pd.isna(acreage_str):
-        return 0
-    else:
-        return float(acreage_str.replace("m2", "").replace(",", "."))
+    return (
+        0
+        if pd.isna(acreage_str)
+        else float(acreage_str.replace("m2", "").replace(".", "").replace(",", "."))
+    )
 
 
 conn = sqlite3.connect("demo_web/test.db")
@@ -28,17 +31,26 @@ with open("quan/bat_dong_san_vn.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
 for item in data:
-    title = item.get("title", "")
-    price = convert_price(item.get("price", ""))
-    acreage = convert_acreage(item.get("acreage", ""))
-    description = item.get("description", "")
-    main_image = item.get("image_url", "")
-    source = "https://batdongsan.vn/"
-
-    cursor.execute(
-        "INSERT INTO products (title, price, acreage, description, main_image, source) VALUES (?, ?, ?, ?, ?, ?)",
-        (title, price, acreage, description, main_image, source),
+    title, price, acreage = (
+        item.get("title", ""),
+        convert_price(item.get("price", "")),
+        convert_acreage(item.get("acreage", "")),
     )
+
+    if acreage > 10:
+        price_per_m2 = (
+            round(price / acreage) if price and acreage and acreage != 0 else 0
+        )
+        description, main_image, source = (
+            item.get("description", ""),
+            item.get("image_url", ""),
+            "https://batdongsan.vn/",
+        )
+
+        cursor.execute(
+            "INSERT INTO products (title, price, acreage, price_per_m2, description, main_image, source) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (title, price, acreage, price_per_m2, description, main_image, source),
+        )
 
 conn.commit()
 conn.close()
